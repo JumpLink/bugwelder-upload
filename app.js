609da5baa73322f4a6da624198bccb433ad67f52
Pascal Garber <jumplink@gmail.com>
2012-08-28 18:05:43 +0200

    v0.0.1

---
+++ app.js
@@ -0,0 +1,59 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , http = require('http')
+  , format = require('util').format
+  , fs = require('fs');
+
+var app = express();
+var upload_path = __dirname + '/public/uploads';
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.static(__dirname + '/public'));
+  app.use(express.static(upload_path));
+  app.use(express.bodyParser({uploadDir: upload_path}));
+  app.use(express.methodOverride());
+  app.use(app.router);
+});
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', function(req, res){
+  res.render('index', { title: 'Datei Upload'});
+});
+
+app.post('/', function(req, res, next){
+  console.log(req.body);
+  console.log(req.files.uploadingFile);
+
+  var uploadingFile = req.files.uploadingFile;
+  var tmpPath = uploadingFile.path;
+  var targetPath = upload_path + '/' + uploadingFile.name;
+
+  fs.rename(tmpPath, targetPath, function(err) {
+    if (err) throw err;
+    fs.unlink(tmpPath, function() {
+      if (err) throw err;
+      // the uploaded file can be found as `req.files.uploadingFile` and the
+      // title field as `req.body.title`
+      res.send(format('\nuploaded %s (%d Kb) to %s as %s'
+        //, req.files.uploadingFile.name
+        , req.files.uploadingFile.name
+        , req.files.uploadingFile.size / 1024 | 0 
+        , req.files.uploadingFile.path
+        , req.body.title));
+    });
+  });
+});
+
+http.createServer(app).listen(3000, '127.0.0.1');
+
+console.log("Express server listening on port 3000");
---
+++ public/javascripts/main.js
@@ -0,0 +1,58 @@
+//main.js
+
+$('#inputFile').on('change', function() {
+	var delimeter = ': ';
+	var file = $('#inputFile').get(0).files[0];
+	$('#fileInfo').show();
+	$('#fileName').text(['Name', file.name].join(delimeter));
+	$('#fileSize').text(['Size', file.size].join(delimeter));
+	$('#fileType').text(['Type', file.type].join(delimeter));
+});
+
+$('#inputSubmit').on('click', function() {
+	var fd = new FormData();
+	fd.append('uploadingFile', $('#inputFile').get(0).files[0]);
+	fd.append('date', (new Date()).toString()); // req.body.date
+	fd.append('comment', 'This is a test.'); // req.body.comment
+
+	var xhr = new XMLHttpRequest();
+	xhr.upload.addEventListener("progress", uploadProgress, false);
+	xhr.addEventListener("load", uploadComplete, false);
+	xhr.addEventListener("error", uploadFailed, false);
+	xhr.addEventListener("abort", uploadCanceled, false);
+	//Neue Seite bei Upload oeffnen
+	//xhr.open("POST", "/upload");
+	xhr.open("POST", "");
+	$('#progress').show();
+	xhr.send(fd);
+});
+
+function uploadProgress(evt) {
+	if(evt.lengthComputable) {
+		var percentComplete = Math.round(evt.loaded * 100 / evt.total);
+		var steps = $('#progress').width() / 100;
+		$('#indicator').text(percentComplete.toString() + '%');
+		$('#indicator').width(Math.round(percentComplete * steps));
+	} else {
+		$('#indicator').text('unable to compute');
+	}
+}
+
+function uploadComplete(evt) {
+	var file = $('#inputFile').get(0).files[0];
+	//uploadProgress(evt);
+	$('#progress').hide();
+	$('#uploade_complete').show();
+	//$('a.download').css('href', document.location.href	+ 'uploads/' + file.name)
+	$('a.download').attr("href", document.location.href	+ 'uploads/' + file.name);
+	$('a.download').text("download file") //
+	alert(evt.target.responseText);
+}
+
+function uploadFailed(evt) {
+	alert("There was an error attempting to upload the file.");
+}
+
+function uploadCanceled(evt) {
+	alert("The upload has been canceled by the user or the browser dropped the connection.");
+}
